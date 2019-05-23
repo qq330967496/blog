f34c38164c0429f8544c94606563c3d92b58d2df
SeverHo <330967496@qq.com>
2019-05-23 09:45:03 +0800

    配置调整

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  base: '/', //部署站点的基础路径，如果你想让你的网站部署到一个子路径下，你将需要设置它。如 Github pages，如果你想将你的网站部署到 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/"，它的值应当总是以斜杠开始，并以斜杠结束。
+  base: '/blog', //部署站点的基础路径，如果你想让你的网站部署到一个子路径下，你将需要设置它。如 Github pages，如果你想将你的网站部署到 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/"，它的值应当总是以斜杠开始，并以斜杠结束。
   dest: './dist',
   title: '公牛开源组织博客',
   description: '公牛开源组织，开源产品Zeus 宙斯权限系统。', //网站的描述，它将会以 <meta> 标签渲染到当前页面的 HTML 中,还显示在首页的文章列表上面
@@ -8,13 +8,13 @@ module.exports = {
       'link',
       {
         rel: 'shortcut icon',
-        href: '/favicon.ico'
+        href: './favicon.ico'
       }
     ]
   ],
   theme: 'indigo-material',
   locales: {
-    '/': {
+    '/blog': {
       lang: 'zh-CN',
       title: '公牛开源组织博客',
       description: '公牛开源组织，开源产品Zeus 宙斯权限系统。'
@@ -29,8 +29,8 @@ module.exports = {
     author: '公牛开源组织博客', //侧边栏的设置
     email: 'open@bullteam.cn',
     pagination: '5', //每一页显示的文章个数
-    avatar: '/avatar.png', //头像地址
-    brand: '/brand.jpg', //头像背景图片地址
+    avatar: './avatar.png', //头像地址
+    brand: './brand.jpg', //头像背景图片地址
     github: 'https://github.com/bullteam', //点击github跳转的地址
     vssue: {
       //评论的配置,
